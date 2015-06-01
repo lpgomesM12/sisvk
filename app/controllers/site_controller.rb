5fe1e5df46eb328b401c5ef7557889dc6bcd73c7
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2015-06-01 17:43:46 -0300

    continuando

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,9 +5,8 @@ class SiteController < ApplicationController
 
      @vk_produtos = VkProduto.find_by_nome_produto_or_all(params[:q])
 
-  #	@q = VkProduto.ransack(params[:q])
-  #	@vk_produtos = @q.result
-
+	  #	@q = VkProduto.ransack(params[:q])
+	  #	@vk_produtos = @q.result
  end
 
  def home
--- app/models/vk_produto.rb
+++ app/models/vk_produto.rb
@@ -18,5 +18,16 @@ class VkProduto < ActiveRecord::Base
     end    
   end
 
+  def self.busca_nome_vitrine(nome,vitrine)
+    if query
+      query = query.downcase
+     self.where('lower(nome_produto)like ?',"%#{query}%") 
+     #self.joins('LEFT JOIN vk_empresavitrines vk on vk_empresa_id = vk.vk_empresa_id').where('(sp.NOME_pessoa) like ? and sp.sivic_igreja_id = ? and sp.data_exclusao is null', "%#{query}%", sivic_igreja_id).order('NOME_pessoa')
+    else
+     self.all
+      #self.joins('LEFT JOIN sivic_pessoas sp on sivic_pessoa_id = sp.id').where('sp.sivic_igreja_id = ? and sp.user_exclusao is null',sivic_igreja_id).order('NOME_pessoa')
+    end  
+  end
+
 
 end
