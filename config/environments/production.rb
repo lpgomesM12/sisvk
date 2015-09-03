4cfbccc7861d28afa09d14c2fb327077c3c93a06
Leandro Pereira Gomes <grupolpgomes@gmail.com>
2015-09-03 13:03:48 -0300

    corrigindo erro

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,7 @@ Sisvk::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.assets.precompile += %w( '.woff', '.eot', '.svg', '.ttf' )
   # Do not dump schema after migrations.
   #config.active_record.dump_schema_after_migration = false
 end
